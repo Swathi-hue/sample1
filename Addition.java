e991ef0814ba37ca56d8a2bc0bfd166079cdc0c3
Swathi-hue <swathi131.m@gmail.com>
2024-11-18 22:26:26 +0530

    Add files via upload

---
+++ Addition.java
@@ -0,0 +1,14 @@
+class Addition{
+int a,b,c;
+public void giveAdd(){
+c=a+b;
+System.out.println("a value is:"+a);
+System.out.println("b value is:"+b);
+System.out.println("c value is:"+c);
+
+}
+
+}
+
+
+
